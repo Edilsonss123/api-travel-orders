38f74827001b3857961d030f6ba7206816467b8f
edilsonss123 <edilson@Edilson>
2024-11-10 18:12:00 -0300

    travel-ordens: ajustando script inicialização

--- data/api/entrypoint.sh
+++ data/api/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#os sleep é a primeira vez que o container do banco subir, ks
+sleep 15;
 # Impede que o script seja executado em caso de erro
 set -e
 LARAVEL_DIR="/var/www/apps/api-travel-orders"
@@ -9,13 +10,15 @@ cd $LARAVEL_DIR
 echo "Instalando dependências do Composer..."
 composer install --no-interaction --prefer-dist
 
-# Gerar a chave de aplicação do Laravel
-echo "Gerando chave de aplicação..."
-php artisan key:generate --no-interaction
 
-# Gerar a chave JWT (se necessário)
-echo "Gerando chave JWT..."
+
+sleep 5;
+
+# Gerar a chave JWT e APP_KEY(se necessário)
+echo "Gerando chave JWT & APP_KEY..."
+php artisan key:generate --no-interaction
 php artisan jwt:secret --no-interaction
+
 
 # Rodar as migrations do banco de dados
 echo "Rodando as migrations..."
@@ -25,5 +28,12 @@ php artisan migrate --no-interaction --force
 echo "Rodando os seeders..."
 php artisan db:seed --no-interaction
 
+# Limpar cache de configuração, rotas e views
+echo "Limpando cache de configuração, rotas e views..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
 # Iniciar o servidor Laravel:
+echo "Iniciando o servidor Laravel..."
 php artisan serve --host 0.0.0.0 --port 8000
